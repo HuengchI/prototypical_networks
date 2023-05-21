c8215dfd89e28572339901a001812e84e1e6d97a
HuengchI <37769009+HuengchI@users.noreply.github.com>
2023-05-21 20:26:08 +0800

    Update train.py
    Finished train and test functions

--- src/train.py
+++ src/train.py
@@ -4,7 +4,9 @@ import torch
 from parser_util import get_parser
 from omniglot_dataset import OmniglotDataset
 from prototypical_batch_sampler import PrototypicalBatchSampler
+from protonet import ProtoNet
 import numpy as np
+from tqdm import tqdm
 
 
 def init_seed(opt):
@@ -57,6 +59,95 @@ def init_lr_scheduler(opt, optim):
     return torch.optim.lr_scheduler.StepLR(optimizer=optim,
                                            gamma=opt.lr_scheduler_gamma,
                                            step_size=opt.lr_scheduler_step)
+
+def save_list_to_file(path, thelist):
+    with open(path, 'w') as f:
+        for item in thelist:
+            f.write("%s\n" % item)
+
+def train(opt, tr_dataloader, model, optim, lr_scheduler, val_dataloader=None):
+    '''
+    Train the model with the prototypical learning algorithm
+    '''
+
+    device='cuda:0' if torch.cuda.is_available() and opt.cuda else 'cpu'
+
+    best_state=None if val_dataloader is None else model.state_dict()
+
+    train_loss=[]
+    train_acc=[]
+    val_loss=[]
+    val_acc=[]
+    best_acc=0
+
+    best_model_path=os.path.join(opt.experiment_root, 'best_model.pth')
+    last_model_path=os.path.join(opt.experiment_root, 'last_model.pth')
+
+    for epoch in range(opt.epochs):
+        print('=== Epoch: {} ==='.format(epoch))
+        tr_iter = iter(tr_dataloader)
+        model.train(mode=True) # sets the model in training mode
+        for batch in tqdm(tr_iter):
+            optim.zero_grad()
+            x, y=batch
+            x,y=x.to(device),y.to(device)
+            model_output=model(x)
+            loss,acc=loss_fn(model_output,target=y,
+                             n_support=opt.num_support_tr)
+            loss.backward()
+            optim.step()
+            train_loss.append(loss.item())
+            train_acc.append(acc.item())
+        avg_loss=np.mean(train_loss[-opt.iterations:])
+        avg_acc=np.mean(train_acc[-opt.iterations:])
+        print('Avg Train Loss: {}, Avg Train Acc: {}'.format(avg_loss, avg_acc))
+        lr_scheduler.step()
+        if val_dataloader is None:
+            continue
+        val_iter=iter(val_dataloader)
+        model.eval() # sets the model to evaluation mode
+        for batch in val_iter:
+            x,y=batch
+            x,y=x.to(device),y.to(device)
+            model_output=model(x)
+            loss,acc=loss_fn(model_output, target=y,
+                             n_support=opt.num_support_val)
+            val_loss.append(loss.item())
+            val_acc.append(acc.item())
+        avg_loss = np.mean(val_loss[-opt.iterations:])
+        avg_acc = np.mean(val_acc[-opt.iterations:])
+        postfix=' (Best)' if avg_acc>=best_acc else '(Best: {})'.format(best_acc)
+        print('Avg Val Loss: {}, Avg Val Acc: {}{}'.format(
+            avg_loss, avg_acc, postfix))
+        if avg_acc >= best_acc:
+            torch.save(model.state_dict(), best_model_path)
+            best_acc=avg_acc
+            best_state=model.state_dict()
+    
+    torch.save(model.state_dict(), last_model_path)
+
+    for name in ['train_loss', 'train_acc', 'val_loss', 'val_acc']:
+        save_list_to_file(os.path.join(opt.experiment_root,
+                                       name+'.txt'), locals()[name])
+
+    return best_state, best_acc, train_loss, train_acc, val_loss, val_acc
+
+def test(opt, test_dataloader, model):
+    device = 'cuda:0' if torch.cuda.is_available() and opt.cuda else 'cpu'
+    avg_acc = list()
+    for epoch in range(10):
+        test_iter = iter(test_dataloader)
+        for batch in test_iter:
+            x, y = batch
+            x, y = x.to(device), y.to(device)
+            model_output = model(x)
+            _, acc = loss_fn(model_output, target=y,
+                             n_support=opt.num_support_val)
+            avg_acc.append(acc.item())
+    avg_acc = np.mean(avg_acc)
+    print('Test Acc: {}'.format(avg_acc))
+
+    return avg_acc
 
 def main():
     options = get_parser().parse_args()
